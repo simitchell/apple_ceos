6341c4db35a15b182d0c6cd6545a17b56a4f1b45
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-12 09:54:07 -0400

    initial commit

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE apple_ceos(
+    id serial primary key,
+    name Text,
+    slug TEXT,
+    first_year_active INTEGER
+);
---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO apple_ceos(name, slug, first_year_active)
+VALUES ('Michael Spindler', 'michaelspindler', 1993),
+	('Gil Amelio', 'gilamelio', 1996),
+	('Mike Markkula', 'mikemarkkula', 1977),
+	('Tim Cook', 'timcook', 2011),
+	('Mike Scott', 'mikescott', 1978),
+	('John Sculley', 'johnsculley', 1983),
+	('Steve Jobs', 'stevejobs', 1976),
+	('Steve Jobs', 'stevejobs', 1997);
